bf869ffd69711a4a4cca38ddd6df36e0e878dc24
Akhila KS <akhila17_ks@yahoo.com>
2011-09-30 23:56:01 +0530

    second commit

---
+++ test/int_/singleton/logger/LoggerSingleTest.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package int_.singleton.logger;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Akhila
+ *
+ */
+public class LoggerSingleTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link int_.singleton.logger.LoggerSingle#getInstance()}.
+	 */
+	@Test
+	public void testGetInstance() {
+		LoggerSingle.getInstance().info("Working successfully!!!");
+		//fail("Not yet implemented");
+	}
+
+}
